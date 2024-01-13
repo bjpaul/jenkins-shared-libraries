3f302e9d46156139a8b5b6dd18521f5ed3685beb
bjpaul <kgec.bijoypaul@gmail.com>
2024-01-13 17:31:23 +0530

    Update factorial.py

--- resources/factorial.py
+++ resources/factorial.py
@@ -1,10 +1,9 @@
-
 def factorial(number: int):
-  if number == 1 or number == 0:
-    return 1
-  else:
-    return number * factorial(number - 1)
+    if number == 1 or number == 0:
+        return 1
+    else:
+        return number * factorial(number - 1)
   
 
 if __name__ == "__main__":
-  print(factorial(5))
+    print(factorial(5))
