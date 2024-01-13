77642b14e576f0cc41bf5e8b9173ed905cdf2c61
bjpaul <kgec.bijoypaul@gmail.com>
2024-01-13 16:45:27 +0530

    Update pythonExecute.groovy

--- vars/pythonExecute.groovy
+++ vars/pythonExecute.groovy
@@ -1,16 +1,18 @@
-pipeline{
-  environment {
-    HOME = "${env.WORKSPACE}"
-  }
-  agent{
-    docker{
-      image 'Python3.6'
+def call(Map param){
+  pipeline{
+    environment {
+      HOME = "${env.WORKSPACE}"
     }
-  }
-  stages{
-    stage{
-      steps{
-        sh "echo 'Hello World'"
+    agent{
+      docker{
+        image 'Python3.6'
+      }
+    }
+    stages{
+      stage{
+        steps{
+          sh "echo 'Hello World'"
+        }
       }
     }
   }
