30dff7d333f7564c77f88c45ef2a410db20f40fa
bjpaul <kgec.bijoypaul@gmail.com>
2024-01-13 17:52:23 +0530

    Update factorial.py

--- resources/factorial.py
+++ resources/factorial.py
@@ -1,3 +1,4 @@
+import sys
 def factorial(number: int):
     if number == 1 or number == 0:
         return 1
@@ -6,4 +7,7 @@ def factorial(number: int):
   
 
 if __name__ == "__main__":
-    print(factorial(5))
+    if len(sys.argv) < 2:
+        print("Provide the number to calculate the factorail and rerun the job")
+        exit(1)
+    print(factorial(int(sys.argv[1])))
