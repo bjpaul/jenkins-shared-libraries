88dfd2ad07c85a98c0e6392feffc7724213a0494
bjpaul <kgec.bijoypaul@gmail.com>
2024-01-13 16:40:28 +0530

    Create pythonExecute.groovy

---
+++ vars/pythonExecute.groovy
@@ -0,0 +1,17 @@
+pipeline{
+  environment {
+    HOME = "${env.WORKSPACE}"
+  }
+  agent{
+    docker{
+      image 'Python3.6'
+    }
+  }
+  stages{
+    stage{
+      steps{
+        sh "echo 'Hello World'"
+      }
+    }
+  }
+}
