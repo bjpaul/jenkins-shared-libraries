d48c0f747cbac1dcf2ab69f7e4ae55809008e3d6
bjpaul <kgec.bijoypaul@gmail.com>
2024-01-13 18:01:07 +0530

    Update pythonExecute.groovy

--- vars/pythonExecute.groovy
+++ vars/pythonExecute.groovy
@@ -17,9 +17,7 @@ def call(Map param){
           script{
             def fileContent = libraryResource('factorial.py')
             writeFile file: 'main.py', text: fileContent
-            sh 'cat main.py'
-            echo 'python main.py "${params.number}"'
-            sh 'python main.py "${params.number}"'
+            sh "python main.py "${params.number}"
           }
         }
       }
